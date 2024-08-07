219e3bd5f0f5ada040c2888438bd7c6696401cf3
Yash <137068938+YashTirpude@users.noreply.github.com>
2024-08-07 17:01:17 +0530

    post and route controllers added

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
+import postRoutes from "./routes/post.routes.js";
 import { v2 as cloudinary } from "cloudinary";
 import connectMongoDB from "./db/connectMongoDB.js";
 
@@ -23,6 +24,7 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
 
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
