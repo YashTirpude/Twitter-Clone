1e4ac82b8642589d7435f14f6481b8b15f0f7956
Yash <137068938+YashTirpude@users.noreply.github.com>
2024-11-26 17:00:12 +0530

    ui update

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,5 +1,5 @@
 import daisyui from "daisyui";
-import daisyUIThemes from "daisyui/src/theming/themes";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
@@ -13,7 +13,6 @@ export default {
       "light",
       {
         black: {
-          ...daisyUIThemes["black"],
           primary: "rgb(29, 155, 240)",
           secondary: "rgb(24, 24, 24)",
         },
